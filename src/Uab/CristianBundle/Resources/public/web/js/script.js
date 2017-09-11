0c6b93628f4663ee81b27a09b1c018641be140aa
conisor <onisor.c@gmail.com>
2017-09-11 07:59:01 -0700

    Fixes and added link to main page

--- src/Uab/CristianBundle/Resources/public/web/js/script.js
+++ src/Uab/CristianBundle/Resources/public/web/js/script.js
@@ -1,12 +1,15 @@
+var firstRun=1;
+
 $(document).ready(function(){
     //$("button").click(function(){
        // $("label").css("color", "red");
        //   $("span").css("color", "brown");
         //  $("p").css("color", "green");
         //  $("h1").css("color", "green");
-    
-
-
+    if (firstRun===1){
+		$(".cristianli").toggleClass("selectedLi");
+		firstRun++;
+	}
 
 $("#demoBtn").click(function myMove() {
     //var elem = document.getElementById("animate");
